f35065910db4b5cf208c0786e4a9351b4846ed2d
Łukasz Kmiecik <60076764+LukasKmiecik@users.noreply.github.com>
2026-04-24 18:49:09 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'https://unpkg.com/three@0.161.0/build/three.module.js';
-
-console.log('APP START');
-console.log('THREE OK:', THREE);
-
-document.getElementById('nazwaObiektu').textContent = 'MODUŁ DZIAŁA';
-document.getElementById('opisObiektu').textContent = 'Import modułu działa poprawnie.';
-document.getElementById('statusModelu').textContent = 'JavaScript modułowy działa';
